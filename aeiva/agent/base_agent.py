5e95ec51202bda8d43ee9f07ee1b2e295f272a26
chatsci <chatsci.ai@gmail.com>
2023-06-04 00:58:08 +0800

    add base classes

---
+++ aeiva/agent/base_agent.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding=utf-8
+""" This module contains the base class for all agent classes.
+
+Copyright (C) 2023 Bang Liu - All Rights Reserved.
+This source code is licensed under the license found in the LICENSE file
+in the root directory of this source tree.
+"""
+from abc import ABC
+
+
+class BaseAgent(ABC):
+    """ This class is the base class for all agent classes.
+    """
+    pass
