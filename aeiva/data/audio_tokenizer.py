03d1de15c7bd380de7b8b74a8e9ba6a4e31f91f6
chatsci <chatsci.ai@gmail.com>
2023-06-06 00:49:45 +0800

    complete a initial version of audio tokenizer

--- aeiva/data/audio_tokenizer.py
+++ aeiva/data/audio_tokenizer.py
@@ -10,9 +10,12 @@ in the root directory of this source tree.
 from abc import ABC, abstractmethod
 from typing import List, Union
 import librosa
+import torch
+import soundfile as sf
 import numpy as np
-from transformers import Wav2Vec2Tokenizer
-from auditok import ADSFactory, AudioRegion
+from transformers import AutoTokenizer, AutoFeatureExtractor, AutoModelForCTC
+from pydub import AudioSegment
+
 
 
 class BaseTokenizer(ABC):
@@ -42,74 +45,74 @@ class LibrosaWrapper:
         y, _ = librosa.load(filepath, sr=self.sr, **kwargs)
         return y
 
-    def decode(self, data: np.ndarray, **kwargs) -> None:
-        librosa.output.write_wav('reconstructed.wav', data, self.sr, **kwargs)
+    def decode(self, data: np.ndarray, filepath: str, **kwargs) -> None:
+        sf.write(filepath, data, self.sr, **kwargs)
 
 
 class HuggingFaceWrapper:
     """
-    A wrapper for Hugging Face's Wav2Vec2Tokenizer to make it compatible with AudioTokenizer.
-
-    The HuggingFaceWrapper works by tokenizing the input audio file using Hugging Face's Wav2Vec2Tokenizer,
-    and then converting each token into its corresponding ID using the Tokenizer's vocabulary.
+    A wrapper for Hugging Face's Tokenizer to make it compatible with AudioTokenizer.
+    The HuggingFaceWrapper works by extracting features from the input audio using Hugging Face's Feature Extractor,
+    and then converting each token into its corresponding ID using the model's logits.
     """
-    def __init__(self, tokenizer: str):
-        self.tokenizer = Wav2Vec2Tokenizer.from_pretrained(tokenizer)
+    def __init__(self, model_name: str):
+        self.model_name = model_name
+        self.model = AutoModelForCTC.from_pretrained(model_name)
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
 
     def encode(self, filepath: str, **kwargs):
-        tokens = self.tokenizer(filepath, **kwargs)
-        return tokens
+        audio = AudioSegment.from_wav(filepath)
+        # Check if the audio is stereo
+        if audio.channels == 2:
+            # Convert to mono
+            audio = audio.set_channels(1)
+        audio.export(filepath, format="wav")
 
-    def decode(self, data, **kwargs):
-        return self.tokenizer.decode(data, **kwargs)
+        audio_data, _ = sf.read(filepath)
+        input_values = self.feature_extractor(audio_data, return_tensors="pt").input_values
+        logits = self.model(input_values).logits[0]
+        pred_ids = torch.argmax(logits, axis=-1)
+        return pred_ids
 
-
-class AuditokWrapper:
-    """
-    A wrapper for Auditok's audio segmentation tool to make it compatible with AudioTokenizer.
-
-    The AuditokWrapper works by segmenting the input audio file using Auditok's ADSFactory and AudioRegion,
-    and then converting each segment into a time-series representation.
-    """
-    def __init__(self, min_dur: float, max_dur: float):
-        self.min_dur = min_dur
-        self.max_dur = max_dur
-
-    def encode(self, filepath: str, **kwargs):
-        asource = ADSFactory.ads(filepath=filepath)
-        region = AudioRegion.load(asource)
-        segments = region.split_and_plot(self.min_dur, self.max_dur)
-        return segments
-
-    def decode(self, data, **kwargs):
-        pass  # Auditok doesn't provide a straightforward way to convert segments back into an audio file
+    def decode(self, pred_ids, **kwargs):
+        time_offset = self.model.config.inputs_to_logits_ratio / self.feature_extractor.sampling_rate
+        outputs = self.tokenizer.decode(pred_ids, output_word_offsets=True)
+        word_offsets = [
+            {
+                "word": d["word"],
+                "start_time": round(d["start_offset"] * time_offset, 2),
+                "end_time": round(d["end_offset"] * time_offset, 2),
+            }
+            for d in outputs.word_offsets
+        ]
+        words = " ".join([d["word"] for d in word_offsets])
+        print(words)
+        return word_offsets
 
 
 class AudioTokenizer(BaseTokenizer):
     """
-    Create an AudioTokenizer that can use either a librosa, Hugging Face's Wav2Vec2Tokenizer, or Auditok.
+    Create an AudioTokenizer that can use either a librosa, or Hugging Face's Audio Tokenizer.
 
     Args:
-        tokenizer (Union[str, LibrosaWrapper, HuggingFaceWrapper, AuditokWrapper]): The tokenizer to use.
+        tokenizer (Union[str, LibrosaWrapper, HuggingFaceWrapper]): The tokenizer to use.
             - If str: The model name or path of a Hugging Face tokenizer
             - If LibrosaWrapper: A wrapper for Librosa's audio processing methods.
             - If HuggingFaceWrapper: A wrapper for Hugging Face's Wav2Vec2Tokenizer.
-            - If AuditokWrapper: A wrapper for Auditok's audio segmentation methods.
 
     Raises:
-        ValueError: If the `tokenizer` argument is not a str, LibrosaWrapper, HuggingFaceWrapper, or AuditokWrapper.
+        ValueError: If the `tokenizer` argument is not a str, a LibrosaWrapper, or a HuggingFaceWrapper.
     """
-    def __init__(self, tokenizer: Union[str, LibrosaWrapper, HuggingFaceWrapper, AuditokWrapper]):
+    def __init__(self, tokenizer: Union[str, LibrosaWrapper, HuggingFaceWrapper]):
         if isinstance(tokenizer, str):
-            self.tokenizer = Wav2Vec2Tokenizer.from_pretrained(tokenizer)
+            self.tokenizer = AutoTokenizer.from_pretrained(tokenizer)
         elif isinstance(tokenizer, LibrosaWrapper):
             self.tokenizer = tokenizer
         elif isinstance(tokenizer, HuggingFaceWrapper):
             self.tokenizer = tokenizer
-        elif isinstance(tokenizer, AuditokWrapper):
-            self.tokenizer = tokenizer
         else:
-            raise ValueError("tokenizer must be a string, a LibrosaWrapper, a HuggingFaceWrapper, or an AuditokWrapper.")
+            raise ValueError("tokenizer must be a string, a LibrosaWrapper, or a HuggingFaceWrapper.")
 
     def encode(self, filepath: str, **kwargs):
         tokens = self.tokenizer.encode(filepath, **kwargs)
@@ -122,18 +125,13 @@ class AudioTokenizer(BaseTokenizer):
 if __name__ == '__main__':
     # Test the AudioTokenizer with LibrosaWrapper
     tokenizer = AudioTokenizer(LibrosaWrapper(22050))
-    tokens = tokenizer.encode('../data/example.wav')
+    tokens = tokenizer.encode('../data/sample_audio.wav')
     print(tokens.shape)
-    tokenizer.decode(tokens)
+    tokenizer.decode(tokens, filepath='../data/reconstructed_sample_audio.wav')
 
     # Test the AudioTokenizer with HuggingFaceWrapper
-    tokenizer = AudioTokenizer(HuggingFaceWrapper('facebook/wav2vec2-base'))
-    tokens = tokenizer.encode('../data/example.wav')
+    tokenizer = AudioTokenizer(HuggingFaceWrapper("facebook/wav2vec2-base-960h"))
+    tokens = tokenizer.encode('../data/sample_audio.wav')
     print(tokens)
-    text = tokenizer.decode(tokens.input_values, skip_special_tokens=True)
+    text = tokenizer.decode(tokens, skip_special_tokens=True)
     print(text)
-
-    # Test the AudioTokenizer with AuditokWrapper
-    tokenizer = AudioTokenizer(AuditokWrapper(0.1, 0.3))
-    segments = tokenizer.encode('../data/example.wav')
-    print([segment.shape for segment in segments])
